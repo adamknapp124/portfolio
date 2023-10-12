babb58c743b8b3d70a03c15164caaebd3203add8
adamknapp124 <aknapp124@gmail.com>
2023-10-12 14:04:34 -0700

    FInished hero section

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -7,38 +7,41 @@ import Link from 'next/link';
 
 const HeroSection = () => {
 	return (
-		<section className="lg:py-16">
-			<div className="grid grid-cols-1 sm:grid-cols-12">
+		<section className="">
+			<div className="flex justify-evenly xl:flex-row flex-col-reverse">
 				<motion.div
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{ duration: 0.5 }}
-					className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
-					<h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
-						<span className="text-transparent bg-clip-text bg-gradient-to-r from-green-100 to-blue-500">
+					className="place-self-center text-center sm:text-left justify-self-start">
+					<h1 className="text-white mb-4 text-4xl xs:text-5xl md:text-7xl lg:text-7xl lg:leading-normal font-extrabold">
+						<span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
 							Hello, I&apos;m{' '}
 						</span>
 						<br></br>
-						<TypeAnimation
-							sequence={['Adam', 1000, 'Web Developer', 1000]}
-							wrapper="span"
-							speed={50}
-							repeat={Infinity}
-						/>
+						<span className="box-border">
+							<TypeAnimation
+								sequence={['Adam', 1000, 'Web Developer', 1000]}
+								wrapper="span"
+								speed={50}
+								repeat={Infinity}
+							/>
+						</span>
 					</h1>
 					<p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-						Turning imagination into interaction.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+						voluptatum.
 					</p>
-					<div>
+					<div className="">
 						<Link
 							href="/contact"
-							className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-green-100 hover:scale-110 text-black font-bold">
+							className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-purple-500 to-blue-400 hover:scale-110 text-white">
 							Hire Me
 						</Link>
 						<Link
 							href="/"
-							className="px-1 inline-block py-1 w-full sm:w-fit rounded-full hover:scale-110 bg-gradient-to-br from-green-100 to-blue-500 hover:bg-slate-800 text-white mt-3">
-							<span className="block bg-[#121212] rounded-full px-5 py-2">
+							className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-500 to-blue-400 hover:scale-110 text-white mt-3">
+							<span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
 								Download CV
 							</span>
 						</Link>
@@ -48,14 +51,15 @@ const HeroSection = () => {
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{ duration: 0.5 }}
-					className="col-span-4 place-self-center mt-4 lg:mt-0">
-					<div className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+					className="place-self-center mt-4 lg:mt-0">
+					<div className="rounded-full w-[250] h-[50]">
 						<Image
-							src="/images/portfolio-photo.png"
+							src="/images/hero-image.png"
 							alt="hero image"
-							className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
-							width={250}
-							height={250}
+							className=""
+							width={400}
+							height={400}
+							priority
 						/>
 					</div>
 				</motion.div>
