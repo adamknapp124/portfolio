437e1d239b7794c080b029b62f274bb9aee601fd
adamknapp124 <aknapp124@gmail.com>
2023-10-15 22:45:52 -0700

    Removed purple balls

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -7,7 +7,6 @@ import Image from 'next/image';
 const EmailSection = () => {
 	return (
 		<section className="grid md:grid-cols-2 mb-2 gap-4 relative ">
-			<div className="absolute w-80 h-80 z-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full blur-lg top-full -left-4 transform -translate-x-1/2 -translate-y-1/2 overflow-hidden"></div>
 			<div className="z-5">
 				<h5 className="text-xl font-bold text-white my-2">
 					Let&apos;s Connect
