6edd7ca550359bc4cdf1fd5019646bd4fe0a614f
adamknapp124 <aknapp124@gmail.com>
2023-11-20 23:12:07 -0800

    added some hovers

--- app/components/MenuOverlay.jsx
+++ app/components/MenuOverlay.jsx
@@ -5,7 +5,9 @@ const MenuOverlay = ({ links }) => {
 	return (
 		<ul className="flex flex-col py-4 items-center z-15">
 			{links.map((link, index) => (
-				<li key={index} className="cursor-pointer">
+				<li
+					key={index}
+					className="cursor-pointer hover:bg-neutral-800 hover:font-semibold w-11/12 rounded-md text-center p">
 					<NavLink key={index} section={link.component} title={link.title} />
 				</li>
 			))}
